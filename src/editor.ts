1459cd8516145f26c3d3215dcde3f6cab75977d9
Denis Koronchik <denis.koronchik@gmail.com>
2018-09-23 22:51:07 +0300

    Add content changed callback

--- src/editor.ts
+++ src/editor.ts
@@ -1,11 +1,14 @@
 import { editor } from 'monaco-editor';
 import { kLangName } from './scs_support';
 
+export type ContentChangedCallback = (value: String) => void;
+
 export class SCsEditor {
   private _container: HTMLElement = null;
   private _editor: editor.IStandaloneCodeEditor = null;
+  private _onContentChanged: ContentChangedCallback = null;
 
-  constructor(container: HTMLElement, value?: string, theme: string = kLangName) {
+  constructor(container: HTMLElement, value?: string, theme: string = kLangName, onContentChanged: ContentChangedCallback = null) {
     this._container = container;
     this._editor = editor.create(this._container, {
       language: kLangName,
@@ -13,8 +16,23 @@ export class SCsEditor {
       automaticLayout: true,
     });
 
+    this._onContentChanged = onContentChanged;
+
+    this._editor.getModel().onDidChangeContent((e: editor.IModelContentChangedEvent) => {
+      if (this._onContentChanged)
+        this._onContentChanged(this.content);
+    });
+
     if (value)
       this.content = value;
+  }
+
+  public get onContentChanged() : ContentChangedCallback {
+    return this._onContentChanged;
+  }
+
+  public set onContentChanged(callback: ContentChangedCallback) {
+    this._onContentChanged = callback;
   }
 
   public get content() : string {
